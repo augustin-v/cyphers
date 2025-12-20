c7f6ab575c11691cc2f09c631bba88c6c1ebc34f
Augustin Villetard <augustin.dev@tutamail.com>
2025-12-20 18:44:02 +0100

    more secure hashing

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
-use std::hash::{DefaultHasher, Hash, Hasher};
-
-use k256::{AffinePoint, Scalar, elliptic_curve::group::prime::PrimeCurveAffine};
+use k256::{
+    AffinePoint, Scalar, elliptic_curve::group::prime::PrimeCurveAffine,
+    elliptic_curve::point::AffineCoordinates,
+};
+use sha2::{Digest, Sha256};
 fn main() {
     let g = AffinePoint::generator();
-    let mut hasher = DefaultHasher::default();
-    b"augustinhandsome".hash(&mut hasher); // here we'd put any shared secret
-    let secret = hasher.finish();
-    let key = g * Scalar::from(secret);
-    println!("My key is the x coordinate in the point : {:?}", key);
+    let secret = Sha256::digest(b"augustinhandsome"); // here we'd put any shared secret
+
+    let bytes16: [u8; 16] = secret[0..16].try_into().unwrap();
+    let scalar = u128::from_be_bytes(bytes16);
+    let key = (g * Scalar::from(scalar)).to_affine();
+
+    println!("key is {:?}", key.x());
 }
